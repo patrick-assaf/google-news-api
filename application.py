43a6876cf8cb7ec2fb24a8821edb2030790b9a49
Patrick Assaf <patrickassaf96@gmail.com>
2020-03-04 22:54:27 -0800

    Move static files to static folder for Flask access

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+# Create an instance of the Flask class
+app = Flask(__name__, static_url_path='')
+
+#Use the route() decorator to tell Flask what URL should trigger our function
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
