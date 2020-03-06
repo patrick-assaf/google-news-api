fc3ac1c511c41ec67bf01d6ae9ef5b841f8108ce
Patrick Assaf <patrickassaf96@gmail.com>
2020-03-05 18:09:37 -0800

    Fetch and display CNN and FOX headlines

--- app.py
+++ app.py
@@ -5,15 +5,20 @@ app = Flask(__name__)
 
 newsapi = NewsApiClient(api_key='2108471b175647ec9f491085b681aafe')
 
-top_headlines = newsapi.get_top_headlines(q='bitcoin', category='business', language='en', country='us')
+cnn_headlines = newsapi.get_top_headlines(sources="cnn", language="en")
+fox_headlines = newsapi.get_top_headlines(sources="fox-news", language="en")
 
 @app.route('/', methods=['GET'])
 def root():
     return app.send_static_file('index.html')
 
-@app.route('/headlines', methods=['GET'])
-def get_headlines():
-    return jsonify({'headlines': top_headlines})
+@app.route('/cnn-headlines', methods=['GET'])
+def get_cnn_headlines():
+    return jsonify({'headlines': cnn_headlines})
+
+@app.route('/fox-headlines', methods=['GET'])
+def get_fox_headlines():
+    return jsonify({'headlines': fox_headlines})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/scripts.js
+++ static/scripts.js
@@ -1,8 +1,8 @@
-var jsonObj;
-var url = '/headlines';
+var cnnObj;
+var cnnURL = '/cnn-headlines';
 
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", url, false);
+xmlhttp.open("GET", cnnURL, false);
 try {
     xmlhttp.send();
 }
@@ -15,17 +15,51 @@ if(xmlhttp.status == 404){
 }
 else if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
     try {
-        jsonObj = JSON.parse(xmlhttp.responseText);
+        cnnObj = JSON.parse(xmlhttp.responseText);
     }
     catch(parseError) {
         alert("Error: Could not parse JSON file");
     }
 }
 
-for(var article in jsonObj.headlines.articles) {
-    var content = '<div class="card">';
-    content += '<img src="' + jsonObj.headlines.articles[article].urlToImage + '">';
-    content += '<div class="container"><h4><b>' + jsonObj.headlines.articles[article].title + '</b></h4>'
-    content += '<p>' + jsonObj.headlines.articles[article].description +'</p></div></div>';
-    document.write(content);
+var cnn_content = '';
+for(var i = 0; i < 4; i++) {
+    cnn_content += '<div class="card">';
+    cnn_content += '<img src="' + cnnObj.headlines.articles[i].urlToImage + '">';
+    cnn_content += '<div class="container"><h4><b>' + cnnObj.headlines.articles[i].title + '</b></h4>'
+    cnn_content += '<p>' + cnnObj.headlines.articles[i].description +'</p></div></div>';
 }
+document.getElementById("cnn").innerHTML = cnn_content;
+
+var foxObj;
+var foxURL = '/fox-headlines';
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", foxURL, false);
+try {
+    xmlhttp.send();
+}
+catch(sendError) {
+    alert(sendError.message);
+}
+
+if(xmlhttp.status == 404){
+    alert("Error: Unable to load file");
+}
+else if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    try {
+        foxObj = JSON.parse(xmlhttp.responseText);
+    }
+    catch(parseError) {
+        alert("Error: Could not parse JSON file");
+    }
+}
+
+var fox_content = '';
+for(var i = 0; i < 4; i++) {
+    fox_content += '<div class="card">';
+    fox_content += '<img src="' + foxObj.headlines.articles[i].urlToImage + '">';
+    fox_content += '<div class="container"><h4><b>' + foxObj.headlines.articles[i].title + '</b></h4>'
+    fox_content += '<p>' + foxObj.headlines.articles[i].description +'</p></div></div>';
+}
+document.getElementById("fox").innerHTML = fox_content;
