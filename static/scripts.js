1e5103c412d31fd4302c93d9a48b85adcaac572b
Patrick Assaf <patrickassaf96@gmail.com>
2020-03-08 19:32:52 -0700

    Add default values to fields in search form

--- static/scripts.js
+++ static/scripts.js
@@ -323,7 +323,13 @@ function search_page() {
             option.name = source;
             x.add(option);
         }
-    }     
+    }
 
+    var today = new Date();
+    var last_week = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
 
+    document.getElementById("to").valueAsDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    document.getElementById("from").valueAsDate = last_week;
+
+    
 }
